86a7c2faef0a5c1b27d86ebae16412b5d17bfe06
Filip Jachura <filjac5882@op.pl>
2018-10-31 22:11:13 +0100

    Readme added

--- app/config/config.php
+++ app/config/config.php
@@ -3,13 +3,13 @@
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
   define('DB_PASS', '');
-  define('DB_NAME', 'ph');
+  define('DB_NAME', 'workers');
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/ph');
+  define('URLROOT', 'http://localhost/workers');
   // Site Name
-  define('SITENAME', 'Pizza Hut');
+  define('SITENAME', 'Workers');
   // App Version
   define('APPVERSION', '1.0.0');
